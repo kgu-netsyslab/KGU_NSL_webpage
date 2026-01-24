314ee3ebffacb7b1647031d0e291d045aeb636e8
tekyung <austen73@kyonggi.ac.kr>
2026-01-24 15:05:00 +0900

    교수님 데이터 파일 변경

--- src/data/ProfessorData.js
+++ src/data/ProfessorData.js
@@ -1,19 +1,19 @@
 // 교수님 데이터 기본 정리
 const professorData = {
-  "name": "김남기",
-  "role": "교수 / SW안전보안전공 주임교수",
-  "image": (process.env.PUBLIC_URL + "/assets/pro_Kim-ver2.jpg"),
-  "contact": {
-    "email": "ngkim@kyonggi.ac.kr",
-    "phone": "031-249-9662",
-    "location": "경기대학교 소프트웨어경영대학"
+  name: "김남기",
+  role: "교수 / SW안전보안전공 주임교수",
+  image: process.env.PUBLIC_URL + "/assets/pro_Kim-ver2.jpg",
+  contact: {
+    email: "ngkim@kyonggi.ac.kr",
+    phone: "031-249-9662",
+    location: "경기대학교 소프트웨어경영대학"
   },
-  "education": [
+  education: [
     "서강대학교 전자계산학 공학사",
     "한국과학기술원 전자계산학 공학석사",
     "한국과학기술원 전자계산학 공학박사"
   ],
-  "interests": [
+  interests: [
     "인공지능(AI)",
     "사물인터넷(IoT)",
     "블록체인",
