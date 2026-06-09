e5d822cc4f48aaebf5b8015e31c93c56d72b9f2a
tekyung <austen73@kyonggi.ac.kr>
2026-06-09 13:49:12 +0900

    석사과정 항목 업데이트: 이미지 추가

--- src/data/StudentsData.js
+++ src/data/StudentsData.js
@@ -4,14 +4,14 @@ const students = {
         { 
             id: 1, 
             name: "곽윤석", 
-            image: "https://placehold.co/300x300/EFEFEF/AAAAAA?text=Master's-level", 
+            image: process.env.PUBLIC_URL + "/assets/Master's_level_kwak.jpg", 
             interests: "Data scientist, Deep learning", 
             type: "석사과정" 
         },
         /*{ 예비 마킹 
             id: 2, 
             name: "김민규", 
-            image: "https://placehold.co/300x300/EFEFEF/AAAAAA?text=Master's-level", 
+            image: "https://placehold.co/300x300/EFEFEF/AAAAAA?text=Master's+level", 
             interests: "인공지능, 머신러닝", 
             type: "석사과정" 
         }*/
