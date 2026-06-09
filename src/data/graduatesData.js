aef1b7e8cefa5dc1a2a2cf304f238c0aa008d481
tekyung <austen73@kyonggi.ac.kr>
2026-06-09 14:32:51 +0900

    졸업생 목록 2차 업데이트

--- src/data/graduatesData.js
+++ src/data/graduatesData.js
@@ -3,7 +3,8 @@ const graduatesData = {
     graduates: [
         { id: 1, year: "2026", name: "전장군", degree: "석사", note: "" },
         { id: 2, year: "2026", name: "김시헌", degree: "학사", note: "" },
-        { id: 3, year: "2016", name: "이우식", degree: "박사", note: "" },
+        { id: 3, year: "2017", name: "정회정", degree: "석사", note: "" },
+        { id: 4, year: "2016", name: "이우식", degree: "박사", note: "" },
     ],
 };
 export default graduatesData;
